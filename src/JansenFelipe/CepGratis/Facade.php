2b6765430d2509852208d7132e0ba6138e05d6a4
Jansen Felipe <jansen.felipe@gmail.com>
2014-10-06 11:36:47 -0300

    Correção Illuminate Facade

--- src/JansenFelipe/CepGratis/Facade.php
+++ src/JansenFelipe/CepGratis/Facade.php
@@ -2,7 +2,7 @@
 
 namespace JansenFelipe\CepGratis;
 
-class Facade extends Illuminate\Support\Facades\Facade\BaseFacade {
+class Facade extends \Illuminate\Support\Facades\Facade {
 
     protected static function getFacadeAccessor() {
         return 'cep_gratis';
