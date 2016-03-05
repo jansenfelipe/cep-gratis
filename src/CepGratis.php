18b19cb6ef00ef6fd6db7b1595de4bda34beb61f
Jansen Felipe <jansen.felipe@gmail.com>
2016-03-05 16:11:18 -0300

    bugfix

--- src/CepGratis.php
+++ src/CepGratis.php
@@ -1,53 +1,53 @@
 <?php
-    namespace JansenFelipe\CepGratis;
+namespace JansenFelipe\CepGratis;
 
-    use Exception;
-    use Goutte\Client;
-    use JansenFelipe\Utils\Utils as Utils;
+use Exception;
+use Goutte\Client;
+use JansenFelipe\Utils\Utils as Utils;
 
-    class CepGratis {
-        /**
-         * Metodo para realizar a consulta
-         *
-         * @throws  Exception
-         * @param   string $cep CEP
-         * @return  array  Endereço
-         */
-        public static function consulta($cep) {
+class CepGratis {
+    /**
+     * Metodo para realizar a consulta
+     *
+     * @throws  Exception
+     * @param   string $cep CEP
+     * @return  array  Endereço
+     */
+    public static function consulta($cep) {
 
-            if (strlen($cep) < 8)
-                throw new Exception('O cep informado não parece ser válido');
+        if (strlen($cep) < 8)
+            throw new Exception('O cep informado não parece ser válido');
 
-            $client		= new Client();
-    		$crawler 	= $client->request('POST', 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm', [
-                'relaxation'  => Utils::unmask($cep),
-                'tipoCEP'     => 'ALL',
-                'semelhante'  => 'N'
-            ]);
+        $client		= new Client();
+        $crawler 	= $client->request('POST', 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm', [
+            'relaxation'  => Utils::unmask($cep),
+            'tipoCEP'     => 'ALL',
+            'semelhante'  => 'N'
+        ]);
 
-            // Realiza a filtragem para que somente a linha que contenha os
-            // dados que queremos possa ser localizada
-            $tr = $crawler->filter(".tmptabela > tr:nth-child(2)");
+        // Realiza a filtragem para que somente a linha que contenha os
+        // dados que queremos possa ser localizada
+        $tr = $crawler->filter(".tmptabela > tr:nth-child(2)");
 
-            // Recebe o endereço obtido através da consulta
-            $endereco = [
-            	'logradouro'	=> $tr->filter("td:nth-child(1)")->html(),
-            	'bairro'		=> $tr->filter('td:nth-child(2)')->html(),
-            	'cidade'		=> $tr->filter('td:nth-child(3)')->html(),
-            	'cep'			=> $tr->filter('td:nth-child(4)')->html()
-            ];
+        // Recebe o endereço obtido através da consulta
+        $endereco = [
+            'logradouro'	=> $tr->filter("td:nth-child(1)")->html(),
+            'bairro'		=> $tr->filter('td:nth-child(2)')->html(),
+            'cidade'		=> $tr->filter('td:nth-child(3)')->html(),
+            'cep'			=> $tr->filter('td:nth-child(4)')->html()
+        ];
 
-            // Remove um dos logradouros, caso a consulta traga mais de um,
-            // como por exemplo: Rua América - Rua G
-            $aux = explode(" - ", $endereco['logradouro']);
-            $endereco['logradouro'] = (count($aux) == 2) ? $aux[0] : $endereco['logradouro'];
+        // Remove um dos logradouros, caso a consulta traga mais de um,
+        // como por exemplo: Rua América - Rua G
+        $aux = explode(" - ", $endereco['logradouro']);
+        $endereco['logradouro'] = (count($aux) == 2) ? $aux[0] : $endereco['logradouro'];
 
-            // Separa a cidade do Estado. Anteriormente estes campos vinham em TD's separadas
-            // agora, vêm juntas, separadas por uma barra
-            $separado = explode('/', $endereco['cidade']);
-            $endereco['cidade'] = $separado[0];
-            $endereco['uf']     = $separado[1];
+        // Separa a cidade do Estado. Anteriormente estes campos vinham em TD's separadas
+        // agora, vêm juntas, separadas por uma barra
+        $separado = explode('/', $endereco['cidade']);
+        $endereco['cidade'] = $separado[0];
+        $endereco['uf']     = $separado[1];
 
-            return str_replace('&nbsp;', '', array_map('htmlentities', array_map('trim', $endereco)));
-        }
+        return str_replace('&nbsp;', '', array_map('htmlentities', array_map('trim', $endereco)));
     }
+}
