6e6d3c8ef8a23829a2f625fbaee9acd83227366a
Douglas Ianitsky <ianitsky@gmail.com>
2019-01-22 10:59:54 -0200

    Fix: Changes on the correios page

--- src/Providers/CorreiosProvider.php
+++ src/Providers/CorreiosProvider.php
@@ -27,15 +27,33 @@ class CorreiosProvider implements ProviderContract
                 $tr = $crawler->filter('table.tmptabela');
 
                 $params['zipcode'] = $cep;
-                $params['street'] = $tr->filter('tr:nth-child(1) td:nth-child(2)')->html();
-                $params['neighborhood'] = $tr->filter('tr:nth-child(2) td:nth-child(2)')->html();
+                $params['street'] = '';
+                $params['neighborhood'] = '';
+                $params['city'] = '';
+                $params['state'] = '';
 
-                $aux = explode('/', $tr->filter('tr:nth-child(3) td:nth-child(2)')->html());
-                $params['city'] = $aux[0];
-                $params['state'] = $aux[1];
+                for ($i = 1; $i <= 3; $i++) {
+                    if ($tr->filter('tr:nth-child('.$i.') th:nth-child(1)')->count() <= 0) {
+                        break;
+                    }
 
-                $aux = explode(' - ', $params['street']);
-                $params['street'] = (count($aux) == 2) ? $aux[0] : $params['street'];
+                    $index = trim($tr->filter('tr:nth-child('.$i.') th:nth-child(1)')->text(), ':');
+                    $value = $tr->filter('tr:nth-child('.$i.') td:nth-child(2)')->text();
+                    switch ($index) {
+                        case 'Logradouro':
+                            $aux = explode(' - ', $value);
+                            $params['street'] = (count($aux) == 2) ? $aux[0] : $value;
+                            break;
+                        case 'Bairro':
+                            $params['neighborhood'] = $value;
+                            break;
+                        case 'Localidade/UF':
+                            $aux = explode('/', $value);
+                            $params['city'] = $aux[0];
+                            $params['state'] = $aux[1];
+                            break;
+                    }
+                }
 
                 return Address::create(array_map(function ($item) {
                     return urldecode(str_replace('%C2%A0', '', urlencode($item)));
